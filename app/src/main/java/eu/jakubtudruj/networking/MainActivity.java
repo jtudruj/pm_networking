340a676fc3fbb006e46d2565cc96c2fd1b425e22
Jakub Tudruj <jtudruj@hotmail.com>
2016-11-13 15:40:49 +0100

    dzialajace pobieranie contentu strony

--- app/src/main/java/eu/jakubtudruj/networking/MainActivity.java
+++ app/src/main/java/eu/jakubtudruj/networking/MainActivity.java
@@ -8,14 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import org.w3c.dom.Text;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
@@ -26,11 +30,9 @@ import static java.lang.System.in;
 public class MainActivity extends AppCompatActivity {
 
     ImageView mainImageView;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+    TextView myTextView;
+
+    final static int BUFFER_SIZE = 2048;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +40,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.mainImageView = (ImageView) findViewById(R.id.img);
-        new DownloadImageTask().execute("http://image.shutterstock.com/z/stock-photo-example-stamp-123670585.jpg");
+        this.myTextView = (TextView) findViewById(R.id.myTextView);
+
+
+        new DownloadImageTask().execute("http://st.depositphotos.com/1023799/2906/v/950/depositphotos_29066941-stock-illustration-grunge-example-rubber-stamp-vector.jpg");
+        new DownloadTextTask().execute("http://stackoverflow.com/questions/6674341/how-to-use-scrollview-in-android");
     }
 
     private InputStream openHttpConnection(String urlString) throws IOException {
@@ -93,6 +99,37 @@ public class MainActivity extends AppCompatActivity {
 
         protected void onPostExecute(Bitmap result) {
             MainActivity.this.mainImageView.setImageBitmap(result);
+        }
+    }
+
+    private String downloadText(String urlString) {
+        String text = "";
+        InputStream inputStream = null;
+        try {
+            inputStream = this.openHttpConnection(urlString);
+            InputStreamReader isr = new InputStreamReader(inputStream);
+            char[] inputBuffer = new char[BUFFER_SIZE];
+            int charRead;
+            while ((charRead = isr.read(inputBuffer))>0) {
+                //convert chars to a String
+                String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                text += readString;
+                inputBuffer = new char[BUFFER_SIZE];
+            }
+            inputStream.close();
+        } catch (Exception e) {
+            Log.e("Downloading text", e.getLocalizedMessage());
+        }
+        return text;
+    }
+
+    private class DownloadTextTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls) {
+            return MainActivity.this.downloadText(urls[0]);
+        }
+
+        protected void onPostExecute(String result) {
+            MainActivity.this.myTextView.setText(result);
         }
     }
 }
